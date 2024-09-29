2f56925f61a9687c6dc3e421f244964834f697d0
Madhushan R.M.S <sadeepm20@gmail.com>
2024-09-29 15:36:18 +0530

    Update inference_node.py

--- yolo_recognition/yolo_recognition/inference_node.py
+++ yolo_recognition/yolo_recognition/inference_node.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import numpy as np
-from custom_msgs.msg import PersonInfo, PersonArray
+from custom_msgs.msg import PersonInfo, PersonArray, Entities
 from visualization_msgs.msg import Marker, MarkerArray
 import math
 import torch
@@ -27,7 +27,7 @@ class CameraSubscriber(Node):
 
         self.sub_ = self.create_subscription(Image, '/zed2_left_camera/image_raw', self.camera_callback, 10)
         self.img_pub_ = self.create_publisher(Image, '/inference_result', 1)
-        self.array_pub_ = self.create_publisher(PersonArray, '/visual_dynamic_obs_array', 1)
+        self.array_pub_ = self.create_publisher(Entities, '/visual_dynamic_obs_array', 1)
         self.marker_pub_ = self.create_publisher(MarkerArray, '/visual_data_markers', 1)
 
         self.lidar_to_cam_opt = np.array([
@@ -60,17 +60,14 @@ class CameraSubscriber(Node):
         mid_point_x = int(img.shape[1] / 2)
         mid_point_y = int(img.shape[0] / 2)
 
-        # array to store dynamic obstacles
-        dynamic_obs_array = PersonArray()
-        dynamic_obs_array.header.frame_id = "rplidar_link"
-        dynamic_obs_array.header.stamp = self.get_clock().now().to_msg()
-
         # Create a marker array for dynamic obstalces to publish
-        marker_array = MarkerArray()
+        # marker_array = MarkerArray()
 
-        detected_ids = set()  # Track detected markers in this frame
+        # detected_ids = set()  # Track detected markers in this frame
 
-        count = 0  # Use this to assign unique IDs
+        # count = 0  # Use this to assign unique IDs
+
+        classes, arr_x, arr_y = [] , [] , []
 
         for r in (results):
             boxes = r.boxes
@@ -80,7 +77,6 @@ class CameraSubscriber(Node):
                 c = box.cls
                 conf = box.conf.item()
                 if (self.model.names[int(c)] == "normal-adult"):
-                # if (self.model.names[int(c)] == "person"):
 
                     x_min = int(b[0])  # Top-left x-coordinate
                     y_min = int(b[1])  # Top-left y-coordinate
@@ -108,58 +104,68 @@ class CameraSubscriber(Node):
                     lidar_x, lidar_y, lidar_z, _ = np.dot(self.lidar_to_cam_opt, [cam_x, cam_y, cam_z, 1])
 
                     # Create marker for visualization in RViz
-                    marker = Marker()
-                    marker.header.frame_id = "rplidar_link"  
-                    marker.header.stamp = self.get_clock().now().to_msg()
-                    marker.type = Marker.CYLINDER
-                    marker.id = count  
-                    marker.action = Marker.ADD
+                    # marker = Marker()
+                    # marker.header.frame_id = "rplidar_link"  
+                    # marker.header.stamp = self.get_clock().now().to_msg()
+                    # marker.type = Marker.CYLINDER
+                    # marker.id = count  
+                    # marker.action = Marker.ADD
 
                     # Set marker position using distance and angle
-                    marker.pose.position.x = lidar_x
-                    marker.pose.position.y = lidar_y
-                    marker.pose.position.z = 0.0
+                    # marker.pose.position.x = lidar_x
+                    # marker.pose.position.y = lidar_y
+                    # marker.pose.position.z = 0.0
 
-                    marker.scale.x = 0.25  
-                    marker.scale.y = 0.25
-                    marker.scale.z = 0.001
+                    # marker.scale.x = 0.25  
+                    # marker.scale.y = 0.25
+                    # marker.scale.z = 0.001
 
-                    marker.color.a = 1.0  # Opacity (1.0 is fully visible)
-                    marker.color.r = 0.0
-                    marker.color.g = 1.0
-                    marker.color.b = 0.0
+                    # marker.color.a = 1.0  # Opacity (1.0 is fully visible)
+                    # marker.color.r = 0.0
+                    # marker.color.g = 1.0
+                    # marker.color.b = 0.0
 
                     # Add marker to the array
-                    marker_array.markers.append(marker)
+                    # marker_array.markers.append(marker)
 
                     # Draw label
                     label = f'D: {distance:.2f}m A: {angle:.2f} deg'
                     cv2.putText(img, label, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 0), 2)
 
-                    detected_ids.add(count)  # Add this marker to detected IDs
-                    self.active_markers[count] = marker  # Update active markers
+                    classes.append(self.model.names[int(c)])
+                    arr_x.append(lidar_x)
+                    arr_y.append(lidar_y)
+
+                    # detected_ids.add(count)  # Add this marker to detected IDs
+                    # self.active_markers[count] = marker  # Update active markers
 
                     # Create custom message
-                    person_info = PersonInfo()
-                    person_info.name = "person_" + str(count) 
-                    person_info.x = lidar_x
-                    person_info.y = lidar_y
-                    dynamic_obs_array.persons_array.append(person_info)
-                    count += 1
+                    # person_info = PersonInfo()
+                    # person_info.name = "person_" + str(count) 
+                    # person_info.x = lidar_x
+                    # person_info.y = lidar_y
+                    # dynamic_obs_array.persons_array.append(person_info)
+                    # count += 1
+
+        entities = Entities()
+        entities.count = len(arr_x)
+        entities.classes = classes
+        entities.x = arr_x
+        entities.y = arr_y
 
         # handle markers that are no longer detected
-        for marker_id in list(self.active_markers.keys()):
-            if marker_id not in detected_ids:
-                # Create a delete marker
-                delete_marker = Marker()
-                delete_marker.id = marker_id
-                delete_marker.action = Marker.DELETE
-                marker_array.markers.append(delete_marker)
-                # Remove from active markers
-                del self.active_markers[marker_id]
+        # for marker_id in list(self.active_markers.keys()):
+        #     if marker_id not in detected_ids:
+        #         # Create a delete marker
+        #         delete_marker = Marker()
+        #         delete_marker.id = marker_id
+        #         delete_marker.action = Marker.DELETE
+        #         marker_array.markers.append(delete_marker)
+        #         # Remove from active markers
+        #         del self.active_markers[marker_id]
 
-        self.marker_pub_.publish(marker_array)
-        self.array_pub_.publish(dynamic_obs_array)
+        # self.marker_pub_.publish(marker_array)
+        self.array_pub_.publish(entities)
 
         img_msg = bridge.cv2_to_imgmsg(img)
         self.img_pub_.publish(img_msg)
