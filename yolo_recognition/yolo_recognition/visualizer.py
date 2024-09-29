f917237d7e786e228fca2355a8dbe8782ec7a415
Madhushan R.M.S <sadeepm20@gmail.com>
2024-09-29 15:36:31 +0530

    Add visualizer node

--- yolo_recognition/yolo_recognition/visualizer.py
+++ yolo_recognition/yolo_recognition/visualizer.py
@@ -5,7 +5,28 @@ from .include.marker_visualizer import MarkerVisualizer
 class Visualizer(Node):
     def __init__(self):
         super().__init__("visualizer")
-        self.marker_visualizer = MarkerVisualizer(self, "/laser_data_array", "/lidar_markers", "rplidar_link")
+
+        self.declare_parameter("subscriber_topic", "/laser_data_array")
+        self.declare_parameter("publisher_topic", "/lidar_markers")
+        self.declare_parameter("marker_frame", "rplidar_link")
+        self.declare_parameter("marker_color", "red")
+
+        self.subscriber_topic = self.get_parameter("subscriber_topic").get_parameter_value().string_value
+        self.publisher_topic = self.get_parameter("publisher_topic").get_parameter_value().string_value
+        self.marker_frame = self.get_parameter("marker_frame").get_parameter_value().string_value
+        self.marker_color = self.get_parameter("marker_color").get_parameter_value().string_value
+
+        self.get_logger().info("Subscriber topic is set to %s" % self.subscriber_topic)
+        self.get_logger().info("Publisher topic is set to %s" % self.publisher_topic)
+        self.get_logger().info("Marker frame is set to %s" % self.marker_frame)
+        self.get_logger().info("Marker color is set to %s" % self.marker_color)
+
+        self.marker_visualizer = MarkerVisualizer(self, 
+                                                  self.subscriber_topic,
+                                                  self.publisher_topic,
+                                                  self.marker_frame,
+                                                  self.marker_color
+                                                  )
 
 def main(args=None):
     rclpy.init(args=args)
